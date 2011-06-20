132bf41a1142e3f4c265e4321feea30b7e83147e
pankajanupam <pankajanupam@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-20 18:07:52 +0000

    update 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/menu-location/trunk@399304 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- menulocation.php
+++ menulocation.php
@@ -3,7 +3,7 @@
 Plugin Name: Menu Location
 Plugin URI: http://pankajanupam.in/wordpress-plugins/
 Description: you can add number of menu location in your theme with it.
-Version: 1.0.1
+Version: 1.0.2
 Author: Pankaj Anupam
 Author URI: http://pankajanupam.in/
 License: GPLv2 or later
@@ -43,23 +43,41 @@ function anu_manage_menu_location(){ ?>
 					<?php settings_fields( 'menu-location-settings-group' ); ?>
 					    <table class="form-table">
 					        <tr valign="top">
-						        <th scope="row">Menu Location <br /><small> You can enter more than one menu location name sapreated by comma.</small></th>
+						        <th scope="row"><strong>Menu Name</strong> <br /><small> You can enter more than one menu location name sapreated by comma.</small></th>
 						        <td><textarea style="width:400px" name="location_option_name" ><?php echo get_option('location_option_name'); ?></textarea></td>
 					        </tr>
+					        <tr></tr>
 					    </table>					    
 					    <p class="submit">
 					    <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
 					    </p>					
 			</form>
+			<div>
+				<?php 
+				
+					if($anu_menu_location = get_option('location_option_name')){ // check menu location set or not
+						$anu_menu_location_array = explode(',',$anu_menu_location); // change string to array 
+						$anu_flag=1; ?>
+						<strong>List of Menu Locations slug </strong><br />
+						<small>Put meun location slug in wp_nav_menu($args). 
+						<br /> Example : &lt;?php wp_nav_menu( array('theme_location'  => 'your-menu-location-slug') ); ?&gt;</small>
+						<br /><br />
+						<ol style='margin-left:60px'>
+					<?php 	//change array to assocated array ('menu-slug'=>'menu location')
+						foreach ($anu_menu_location_array as $val){								
+							echo '<li>'.str_ireplace(' ','-',trim($val)).'</li>';
+						}	
+						echo "</ol>";
+					}
+				?>
+			</div>
 	</div>
 <?php }
 
-if($anu_menu_location = get_option('location_option_name')){ // check menu location set or not
-	
-	$anu_menu_location_array = explode(',',$anu_menu_location); // change string to array
-	
+if($anu_flag==1){ // check menu location set or not
+		
 	//change array to assocated array ('menu-slug'=>'menu location')
-	foreach ($anu_menu_location_array as $val){	$anu_menu_location_array_acc[str_ireplace(' ','-',$val)] = $val; }
+	foreach ($anu_menu_location_array as $val){	$anu_menu_location_array_acc[str_ireplace(' ','-',trim($val))] = trim($val); }
 	
 	unset($anu_menu_location_array);
 	if(isset($anu_menu_location_array_acc)){ register_nav_menus($anu_menu_location_array_acc); }	
